48a08cee907b761aeca04d1a0ccf2b391f223b8a
Drake Damon <dddamon06@gmail.com>
2024-12-30 17:04:43 -0500

    completed lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,84 @@
 class Coffee:
     def __init__(self, name):
-        self.name = name
+        self._name = name
+        if not isinstance(name, str):
+            raise Exception("Name must be a string")
+        if len(name) <= 2:
+            raise Exception("Name must be longer than 2 characters")
+
+
+    @property
+    def name(self):
+        return self._name
+    
+
         
     def orders(self):
-        pass
+        return [order for order in Order.all if order.coffee == self]
+        
     
     def customers(self):
-        pass
+        customers_orders = self.orders()
+        return list(set([order.customer for order in customers_orders]))
     
     def num_orders(self):
-        pass
+        customers_orders = self.orders()
+        return len(customers_orders)
+
     
     def average_price(self):
-        pass
+        customers_orders = self.orders()
+        return sum([order.price for order in customers_orders]) / len(customers_orders)
 
 class Customer:
     def __init__(self, name):
-        self.name = name
+    
+        if not isinstance(name, str):
+            raise Exception("Name must be a string")
+        
+        if len(name) < 1 or len(name) > 15:
+            raise Exception("Name must be between 1 and 15 characters") 
+        self._name = name
+
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter 
+    def name(self, value):
+        if not isinstance(value, str):
+            raise Exception("Name must be a string")
+        if len(value) < 1 or len(value) > 15:
+            raise Exception("Name must be between 1 and 15 characters") 
+        self._name = value
+
+
         
     def orders(self):
-        pass
+        return [order for order in Order.all if order.customer == self]
     
     def coffees(self):
-        pass
+        customers_orders = self.orders()
+        return list(set([order.coffee for order in customers_orders]))
     
     def create_order(self, coffee, price):
-        pass
-    
+        return Order(self, coffee, price)    
 class Order:
+    all = []
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
-        self.price = price
+        self._price = price
+        Order.all.append(self)
+
+    @property
+    def price(self):
+        return self._price
+
+
+    @price.setter 
+    def price(self, value):
+        if not isinstance(value, (int, float)):
+            raise Exception("Price must be a number")
+
